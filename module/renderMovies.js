a6ef3fbda9c9113950f7ff45272def95a69afc34
Peter Okorafor <peterdtitan@gmail.com>
2023-03-23 11:05:31 +0200

    Finish display of movie items

--- module/renderMovies.js
+++ module/renderMovies.js
@@ -1,23 +1,20 @@
-const renderMovies = document.querySelector('#movies');
+const renderMovies = document.querySelector('#movie-list');
 
 export const exteralApicall = async () => {
   const getData = await fetch('https://api.tvmaze.com/search/shows?q=thriller');
   const request = await getData.json();
   const movieArray = Array.from(request);
   const movieHTML = movieArray.map((item) => `
-<div class="grid" id="${item.show.id}">
-  <div>
-    <div class="image">
-      <img src="${item.show.image.medium}" alt="img1" width="300px">
+  <div class="movie-card">
+    <div class="movie-background" style="background-image: url('${item.show.image.medium}'); background-position: center; background-size: cover;">
+        <button class="watch-cta">Watch Now</button>
     </div>
-    <div class="likes" id="name-likes">
-      <span>${item.show.name}</span>
-    </div>
-    <div class="btn">
-      <button class="comments-btn" data-show="${item.show.id}">Comments</button>
+    <div class="movie-body">
+        <h2 class="movie-title">${item.show.name.length > 20 ? item.show.name.slice(0,20) + '...' : item.show.name}</h2>
+        <button id="comment-btn">Comments</button>
+        <p id="movie-description">Lorem ipsum sjdfd sfjsjdsksd sdjfdkfsdf dsfjsdf</p>
     </div>
   </div>
-</div>
 `);
   renderMovies.innerHTML = movieHTML;
 
@@ -25,3 +22,5 @@ export const exteralApicall = async () => {
 };
 
 export default exteralApicall;
+
+
