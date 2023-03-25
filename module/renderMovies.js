53cd905dd2844450a6a3faf70c0d7e40eff5ed7c
Peter Okorafor <peterdtitan@gmail.com>
2023-03-25 07:54:13 +0200

    Update likes function

--- module/renderMovies.js
+++ module/renderMovies.js
@@ -1,5 +1,6 @@
 import CommentsPopup from './commentPopup';
 import initModal from './closeCommentPopUp';
+import InvolvementAPI from './likeMovie';
 
 const renderMovies = document.querySelector('#movie-list');
 
@@ -13,15 +14,31 @@ export const exteralApicall = async () => {
         <button class="watch-cta">Watch Now</button>
     </div>
     <div class="movie-body">
-        <h2 class="movie-title">${item.show.name.length > 20 ? `${item.show.name.slice(0, 20)} + '...'` : item.show.name}</h2>
-        <button class="comment-btn" id="comment-btn" data-show="${item.show.id}">Comments</button>
-        <p id="movie-description">Lorem ipsum sjdfd sfjsjdsksd sdjfdkfsdf dsfjsdf</p>
+        <h2 class="movie-title">${item.show.name.length > 20 ? `${item.show.name.slice(0, 20)}...` : item.show.name}</h2>
+
+        <div class="interactions">
+          <div class="likes">
+            <p class="total-likes" data-show="${item.show.id}"></p>
+            <button class="like-btn" id="like-btn" data-show="${item.show.id}">Like</button>
+          </div>
+          <button class="comment-btn" id="comment-btn" data-show="${item.show.id}">Comments</button>
+        </div>
+        <p id="movie-description">${item.show.summary.replaceAll('<p>', '').slice(0, 155)}...</p>
     </div>
   </div>
 `);
   renderMovies.innerHTML = movieHTML;
 
   await CommentsPopup.getMovie();
+  await InvolvementAPI.likeMovie()
+
+  const totalLikes = document.querySelectorAll('.total-likes');
+  totalLikes.forEach(async (like) => {
+    const id = like.dataset.show;
+    const currentItem = await InvolvementAPI.getLikesByMovie(id);
+    like.innerHTML = `${currentItem} likes`;
+  });
+
   initModal();
 };
 
